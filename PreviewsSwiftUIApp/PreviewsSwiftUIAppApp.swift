33b6045743c908d3c3bde09daa3230484fce53e3
Cihan Çallı <cihan.callii@gmail.com>
2024-04-02 10:49:23 +0300

    first commit

---
+++ PreviewsSwiftUIApp/PreviewsSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewsSwiftUIAppApp.swift
+//  PreviewsSwiftUIApp
+//
+//  Created by Cihan on 19.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PreviewsSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
